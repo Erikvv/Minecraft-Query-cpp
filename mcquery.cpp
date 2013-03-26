62849f54900b1ac4302647a5a18a70508392310e
Erik van Velzen <erik@evanv.nl>
2013-03-27 00:28:00 +0100

    added timeout

--- mcquery.cpp
+++ mcquery.cpp
@@ -1,4 +1,5 @@
-// g++ mcquery.cpp -o mcquery.run -lboost_system -pthread -lboost_thread
+// Compiles on my debian system with:
+// g++ mcquery.cpp -o mcquery.run -lboost_system -pthread -lboost_thread -std=c++11
 #include <boost/asio.hpp>
 #include <iostream>
 
@@ -26,7 +27,7 @@ public:
 private:
 };*/
 
-mcData mcQuery(const string& ip, const string& port) {
+mcData mcQuery(const string& ip, const string& port, int timeoutsecs = 5) {
    try {
       udp::resolver::query query(ip::udp::v4(), ip, port);
       udp::endpoint mcserverEndpoint = *resolver.resolve(query);
@@ -35,21 +36,29 @@ mcData mcQuery(const string& ip, const string& port) {
       udp::socket mcSocket(ioService);
       mcSocket.open(udp::v4());
 
-      char req[] =  { 0xFE, 0xFD, 0x09, 0x00, 0x00, 0x00, 0x0F };
+      unsigned char req[] =  { 0xFE, 0xFD, 0x09, 0x00, 0x00, 0x00, 0x0F };
       cout<< "sending..." << endl;
-      size_t len = mcSocket.send_to(buffer(req), mcserverEndpoint);
+      size_t len = mcSocket.send_to(buffer(req), mcserverEndpoint);  // UDP: doesn't need to be async
       cout<< "sent " << len << " bytes" << endl;
+
+      deadline_timer t(ioService, boost::posix_time::seconds(timeoutsecs));
+      t.async_wait([&](const boost::system::error_code& e) {
+            cout<< "timed out: closing socket";
+            mcSocket.close();
+            } );
 
       cout<< "preparing recieve buffer" << endl;
       char recvBuffer[16] = { 0 };
       udp::endpoint thisEndpoint;
       cout<< "waiting on response..." << endl;
       len = mcSocket.receive_from(buffer(recvBuffer), thisEndpoint);
-
       cout<< "recieved " << len << " bytes" << endl;
+
+      mcSocket.async_connect(
+
       // byte 0 is 0x09
-      // byte 1 to 4 is the session id (last 4 bytes of the request above)
-      // byte 5 onwards is a null-terminated big endian ASCII number string which should be converted to an integer
+      // byte 1 to 4 is the session id (last 4 bytes of the request above). We don't use this
+      // byte 5 onwards is the challange token: a null-terminated big endian ASCII number string which should be converted to an integer
       int challtoken = atoi(&recvBuffer[5]);    // undefined behavior when string is incorrect
       cout<< "challenge token: " << challtoken << endl;
    }
